7ea9996ad1165e0e590f0104fddd97e58d6fa99e
Meinhard Dam <meinhard.dam@gmail.com>
2014-03-14 10:55:10 +0100

    start of geolocation

--- WebContent/script.js
+++ WebContent/script.js
@@ -155,6 +155,7 @@ $(function() {
 		}
 	});
 });
+
 /*
  * This is the central object in our JavaScript. It contains all the information we need above the customer. It is used throughout our JavaScript code!
  */
@@ -174,6 +175,7 @@ function resetFields() {
 	$("#usernameField").val("");
 	$("#passwordField").val("");
 }
+
 /*
  * This function adds the items on our server to our view. It also adds the add to cart button and its event handler!
  */
@@ -425,6 +427,43 @@ function handleDrop(event) {
 
 function allowDrop(ev) {
 	ev.preventDefault();
+}
+
+/*
+ * Code for geolocation
+ */
+$(function getLocation()
+{
+if (navigator.geolocation)
+  {
+  navigator.geolocation.getCurrentPosition(showPosition, showError);
+  }
+else{alert("Geolocation is not supported by this browser.");}
+});
+
+function showPosition(position)
+{
+alert("Latitude: " + position.coords.latitude + 
+"<br>Longitude: " + position.coords.longitude); 
+}
+
+function showError(error)
+{
+switch(error.code) 
+  {
+  case error.PERMISSION_DENIED:
+    alert("User denied the request for Geolocation.");
+    break;
+  case error.POSITION_UNAVAILABLE:
+    alert("Location information is unavailable.");
+    break;
+  case error.TIMEOUT:
+    alert("The request to get user location timed out.");
+    break;
+  case error.UNKNOWN_ERROR:
+    alert("An unknown error occurred.");
+    break;
+  }
 }
 
 /////////////////////////////////////////////////////
